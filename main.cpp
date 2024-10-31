f54f5174dedc0e9a87d54df3f4b334c1f556c735
chz2012 <53894684+chz2012@users.noreply.github.com>
2024-10-31 10:17:09 +0800

    开发：项目初始化

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
+    // std::cout << "Hello, World!" << std::endl;
+    // std::cout << "Hello, World!" << std::endl;
+    // std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
