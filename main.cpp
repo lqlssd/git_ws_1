2f39aa66c92fb93a4da627a163fe3a068b82d14e
lqlssd <2749646519@qq.com>
2024-10-31 11:04:51 +0800

    开发：李秋灵提交

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     std::cout << "Hello, World!" << std::endl;
     // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
-    // std::cout << "Hello, World!" << std::endl;
+    std::cout << "This change frome lql " << std::endl;
     // std::cout << "Hello, World!" << std::endl;
     // std::cout << "Hello, World!" << std::endl;
     return 0;
